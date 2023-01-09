1cb17bb3731d40cdcda23de6259828c74bfddee9
Frankenstei <enwereloveday@yahoo.com>
2023-01-08 21:07:27 -0800

    11

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == "":
-        return (0, None))
+        return (0, None)
     elif:
         return ((len(sentence), sentence[0]))
