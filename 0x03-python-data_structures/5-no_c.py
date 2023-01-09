beaf7d84dd3bfa50a8ab7684be664ad18020fcf3
Frankenstei <enwereloveday@yahoo.com>
2023-01-08 20:18:47 -0800

    11

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in range(len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            del my_string[i]
-    return my_string
+        copy = [x for x in my_string if x != 'c' and x != 'C']
+        return ("".join(copy))
