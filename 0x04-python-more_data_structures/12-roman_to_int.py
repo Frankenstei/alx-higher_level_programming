9bdffb60204bd2cd6e41c15e2c33217d0d6b7519
Frankenstei <enwereloveday@yahoo.com>
2023-01-11 14:56:45 -0800

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or roman_string in None):
+    value = 0
+    if not (isinstance(roman_string, str)):
         return (0)
-    dict_roman = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    number = 0;
-    for i in range(len(roman_string):
-            if (dict_roman.get(roman_string[i], 0) == 0:
-                return (0)
-            if (i != len(roman_string) - 1) and
-                dict_roman[roman_string[i]] < dict_roman[roman_string[i + 1]]):
-            number = number + dict_roman[roman_string[i]] * -1
-            else:
-                number += dict_roman[roman_string[i]]
-    return (nummber)
+    for i in range(len(roman_string)):
+        if (roman_string[i] == 'I'):
+            value += 1
+        if (roman_string[i] == 'V'):
+            if (roman_string[i - 1] == 'I' and i != 0):
+                value -= 2
+            value += 5
+        if (roman_string[i] == 'X'):
+            if (roman_string[i - 1] == 'I' and i != 0):
+                value -= 2
+            value += 10
+        if (roman_string[i] == 'L'):
+            if (roman_string[i - 1] == 'X' and i != 0):
+                value -= 20
+            value += 50
+        if (roman_string[i] == 'C'):
+            if (roman_string[i - 1] == 'X' and i != 0):
+                value -= 20
+            value += 100
+        if (roman_string[i] == 'D'):
+            if (roman_string[i - 1] == 'C' and i != 0):
+                value -= 200
+            value += 500
+        if (roman_string[i] == 'M'):
+            if (roman_string[i - 1] == 'C' and i != 0):
+                value -= 200
+            value += 1000
+            return (value)
