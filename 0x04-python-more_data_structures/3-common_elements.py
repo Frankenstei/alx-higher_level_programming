5438a4ccc83be58199a70b080a5f577fbc476431
Frankenstei <enwereloveday@yahoo.com>
2023-01-11 13:08:30 -0800

    set combined

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_comb = []
-    for i in range(len(set_1)):
-        for j in range(len(set_2)):
-            if set_1[i] == set_2[j]:
-                set_comb.append(set_2[j])
-    return (set_comb)
+    return (set_1 & set_2)
