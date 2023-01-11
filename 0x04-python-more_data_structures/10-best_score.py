b4de6f62fb5780b370229f242688bf4d0d1cb0a3
Frankenstei <enwereloveday@yahoo.com>
2023-01-11 13:40:31 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,7 @@
 def best_score(a_dictionary):
     if not (a_dictionary):
         return (None)
+    biggest = max(a_dictionary.values())
     for key in a_dictionary.keys():
-        if (a_dictionary[key] = max(a_dictionary.values())):
+        if (a_dictionary[key] = biggest):
             return (key)
