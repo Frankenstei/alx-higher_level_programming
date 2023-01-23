ca8054d0987e18bea8fc050c54c695b75d1c6e01
Frankenstei <enwereloveday@yahoo.com>
2023-01-23 05:15:18 -0800

    0 task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    no_of_elements = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end = "")
+            no_of_elements = no_of_elements + 1
+        except IndexError:
+            break
+    return (no_of_elements)
