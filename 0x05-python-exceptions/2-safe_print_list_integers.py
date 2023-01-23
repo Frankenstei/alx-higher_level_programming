d6b1d38ebf676f6c339c3c0e476312014701593d
Frankenstei <enwereloveday@yahoo.com>
2023-01-23 07:02:32 -0800

    2 task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     no_of_elements = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end = "")
+            print("{:d}".format(my_list[i]), end="")
             no_of_elements += 1
         except (ValueError, IndexError):
             continue
