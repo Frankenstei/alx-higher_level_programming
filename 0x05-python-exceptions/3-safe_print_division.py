9c61874c5c2755774233c5c6b0d1a0fbbcbc11c7
Frankenstei <enwereloveday@yahoo.com>
2023-01-23 09:31:17 -0800

    3 task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@
 def safe_print_division(a, b):
     try:
         div = a / b
+        return (div)
     except (TypeError, ZeroDivisionError):
         return (None)
     finally:
        print("Inside result: {}".format(div))
-    return (div)
