6ed93063b8bceafa580add6ece411768a0ab6d3e
Frankenstei <enwereloveday@yahoo.com>
2023-01-23 09:20:20 -0800

    2 task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     no_of_elements = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             no_of_elements = no_of_elements + 1
