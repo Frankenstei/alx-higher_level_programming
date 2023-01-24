05bdcab2141ca4ab65de3687784c9eeedc5cfc66
Frankenstei <enwereloveday@yahoo.com>
2023-01-24 12:57:47 -0800

    3 task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""square class"""
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """initializing size:
+
+        Args:
+           size(int): must be integer
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return (self.__size * self.__size)
