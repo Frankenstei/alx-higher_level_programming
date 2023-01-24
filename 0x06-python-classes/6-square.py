aba9ed24b286a8e497d1b96c2fddc189619c7352
Frankenstei <enwereloveday@yahoo.com>
2023-01-24 14:51:54 -0800

    6 task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,14 @@ class Square:
     """square class"""
 
     def __init__(self, size):
-        """initializing size:
+        """initializing size.
 
         Args:
           size(int): must be integer
+          position (int, int): position
         """
         self.size = size
+        self.position = position
 
         @property
         def size(self):
@@ -19,11 +21,25 @@ class Square:
 
         @size.setter
         def size(self, value):
-            if not isinstance(size, int):
+            if not isinstance(value, int):
                 raise TypeError("size must be an integer")
-            elif size < 0:
+            elif value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
+
+            @property
+            def position(self):
+                """get current position"""
+                return (self.__position)
+
+            @position.setter
+            def position(self, value):
+                if (not isinstance(value, tuple) or 
+                        len(value != 2) or 
+                        not all(isinstance(num, int) for num in value)
+                        not all(num >= 0 for num in value)):
+                    raise TypeError("position must be a tuple of 2 positive integers")
+                self.__position = value
 
         def area(self):
             """ return area """
@@ -31,8 +47,12 @@ class Square:
         
         def my_print(self):
             """ prints # squares """
-            for i in range(0, self.__size):
-                [print("#", end="") for j in range(self.__size)]
+            if self.__size == 0:
                 print("")
-                if self.__size == 0:
-                    print("")
+                return
+
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(self.__size)]
+                print("")
