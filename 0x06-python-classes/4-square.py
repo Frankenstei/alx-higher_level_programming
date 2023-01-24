f2b64ee395568e0cc029d2d14146f8b4b83335e0
Frankenstei <enwereloveday@yahoo.com>
2023-01-24 13:41:19 -0800

    4 task

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""square and private instance atteibute"""
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """initializing size:
+
+        Args:
+          size(int): must be integer
+        """
+        self.__size = size
+
+        @property
+        def size(self):
+            return (self.__size)
+        @size.setter
+        def size(self, value):
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        def area(self):
+            return (self.__size * self.__size)
