e2250add6f58bf219ec9d747d8f5704a7ab1f384
Frankenstei <enwereloveday@yahoo.com>
2023-01-24 11:23:54 -0800

    1 task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""size is private"""
+
+class Square:
+    """size is private"""
+    def __init__(self, size):
+        """initialize square
+        Args:
+         size (int): size
+         """
+        self.__size = size
