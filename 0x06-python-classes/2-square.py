00c15771b037c463151158877073ee0a8c8dd56a
Frankenstei <enwereloveday@yahoo.com>
2023-01-24 11:40:20 -0800

    2 task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""exception in class"""
+
+class Square:
+    """square class"""
+    def __init__(self, size = 0):
+        """size initialization
+        
+        Args:
+           size (int): must be integer
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
