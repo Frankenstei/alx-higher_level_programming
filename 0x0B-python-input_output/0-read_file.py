9b6fb9bcbb3516cb1f92f02f0c9f1b5a46bb84fc
Frankenstei <enwereloveday@yahoo.com>
2023-02-07 08:00:21 -0800

    readfile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
+#0-read_file.py
 """reads text file, prints to stdout"""
 
 def read_file(filename=""):
     """prints file to stdout"""
-    with open(filename, encoding="utf-8"):
-        for line in filename as f:
-            print(f.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
