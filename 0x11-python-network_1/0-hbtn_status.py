6940eda2acf81c84e7bc488a5ebe06f4680e1e6b
Frankenstei <enwereloveday@yahoo.com>
2023-04-28 09:33:39 -0700

    all

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-     fetches https://intranet.hbtn.io/status
+Write a Python script that fetches https://intranet.hbtn.io/status
 """
-
-from urllib import (request)
+import urllib.request
 
 if __name__ == "__main__":
-    req = request.Request("https://intranet.hbtn.io/status")
-    with request.urlopen(req) as response:
-        body = response.read()
-
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
