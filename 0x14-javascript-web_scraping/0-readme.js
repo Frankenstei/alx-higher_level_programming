4dda5a1b7e7113bd265a93d9e7a802655526dcce
Frankenstei <enwereloveday@yahoo.com>
2023-05-23 14:37:26 -0700

    all

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-let fs = require('fs');
-let file = process.argv[2];
-
-function readPrint (file) {
-  fs.readFile(file, 'utf-8', function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
-  });
-}
-readPrint(file);
+const fs = require('fs');
+const filename = process.argv[2];
+fs.readFile(filename, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
